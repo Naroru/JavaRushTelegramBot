1d35619553b5962ab51aaed2d1f4e6911911dd24
Oleg Chukhlantsev <Nerevar5@mail.ru>
2024-03-09 09:25:03 +0300

    JRTB-№: Z

--- src/test/java/com/github/Naroru/JavaRushTelegramBot/JavarushTelegrambotApplicationTests.java
+++ src/test/java/com/github/Naroru/JavaRushTelegramBot/JavarushTelegrambotApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.Naroru.JavaRushTelegramBot.jrtb;
+package com.github.Naroru.JavaRushTelegramBot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
