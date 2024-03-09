d56639713d7823091d526c7facb497160fd760bd
Oleg Chukhlantsev <Nerevar5@mail.ru>
2024-03-09 09:43:49 +0300

    JRTB-№: 1231

--- src/main/java/com/github/Naroru/JavaRushTelegramBot/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/Naroru/JavaRushTelegramBot/JavarushTelegramBotApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavarushTelegramBotApplication {
 
-int t = 1333;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavarushTelegramBotApplication.class, args);
