cba49acb522acce49d14946ce2928ad50cde46f1
Naroru <Nerevar5@mail.ru>
2024-04-03 11:51:27 +0300

    JRTB-1: added test migration. Doesn't work. Pulled for stackOverflow

---
+++ src/main/resources/db/migration/V01__test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tg_user
+(
+    chat_id VARCHAR(100) PRIMARY KEY,
+    active  BOOLEAN
+);
---
+++ src/main/resources/db/migration/V02_test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tg_user_test
+(
+    chat_id VARCHAR(100) PRIMARY KEY,
+    active  BOOLEAN
+);
