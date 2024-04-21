69e793c99900f99a23561832f2536b036985f34b
Naroru <Nerevar5@mail.ru>
2024-04-21 13:26:38 +0300

    CI: restore correct app.props

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ export BOT_NAME=$1
 export BOT_TOKEN=$2
 export BOT_DB_USERNAME='prod_jrtb_db_user'
 export BOT_DB_PASSWORD='Pap9L9VVUkNYj99GCUCC3mJkb'
+export BOT_DB_URL= 'jdbc:postgresql://jrtb-db:5432/jrtb_db'
 
 # Prepare Jar
 mvn clean
